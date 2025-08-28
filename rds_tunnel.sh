fd22d9213ce04a349f1df2b9f1e0e0a5092827b2
tony.li <tony.li@nothingphisphy.com>
2025-08-28 14:44:58 +1000

    update to get ip dunamically

--- rds_tunnel.sh
+++ rds_tunnel.sh
@@ -2,20 +2,32 @@
 
 # Configuration variables
 EC2_USER="ec2-user"
-EC2_IP="52.64.95.232"  # Current IP, will need to be updated when EC2 restarts
+EC2_INSTANCE_ID="i-06d05987f082b60c9"  # Replace with your EC2 instance ID
 EC2_KEY_PATH="~/Tony/project/aws/my-app.pem"
-RDS_HOST="database-1.cx0qnfjtc2ft.ap-southeast-2.rds.amazonaws.com"
+#RDS_HOST="products-api-rds-dev-db.cx0qnfjtc2ft.ap-southeast-2.rds.amazonaws.com"
+RDS_HOST="products-api-rds-dev-db.cx0qnfjtc2ft.ap-southeast-2.rds.amazonaws.com"
 RDS_PORT="5432"
 LOCAL_PORT="5433"
+
+# Get EC2 public IP dynamically
+echo "Getting EC2 public IP..."
+EC2_IP=$(aws ec2 describe-instances \
+  --instance-ids $EC2_INSTANCE_ID \
+  --query 'Reservations[0].Instances[0].PublicIpAddress' \
+  --output text)
+
+if [ "$EC2_IP" = "None" ] || [ -z "$EC2_IP" ]; then
+  echo "Error: Could not get public IP for instance $EC2_INSTANCE_ID"
+  echo "Make sure the instance is running and has a public IP"
+  exit 1
+fi
+
+echo "Found EC2 public IP: $EC2_IP"
 
 # Create SSH tunnel
 echo "Setting up SSH tunnel to RDS through EC2 jump box..."
 echo "RDS will be available at localhost:$LOCAL_PORT"
 echo "Press Ctrl+C to close the tunnel when finished"
-
-# Ask for current EC2 IP if needed
-read -p "Use EC2 IP [$EC2_IP]? Press Enter to accept or type a new IP: " input_ip
-EC2_IP=${input_ip:-$EC2_IP}
 
 # Fix permissions on the key file
 echo "Setting correct permissions on SSH key..."
