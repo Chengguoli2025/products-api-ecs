dc2c37385ec6937d25e6b985ed66ec667fae734b
tony.li <tony.li@nothingphisphy.com>
2025-09-18 12:09:12 +1000

    Add ip address

--- src/main/java/com/tony/products/controller/HealthController.java
+++ src/main/java/com/tony/products/controller/HealthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -14,10 +15,18 @@ public class HealthController {
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> healthCheck() {
+        String ipAddress;
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            ipAddress = "unknown";
+        }
+        
         return ResponseEntity.ok(Map.of(
             "status", "OK",
             "version", "v6",
-            "time", LocalDateTime.now()
+            "time", LocalDateTime.now(),
+            "ipAddress", ipAddress
         ));
     }
 
